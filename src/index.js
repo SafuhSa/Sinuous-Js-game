606cccd1eb7fe58608a7c29488f2ef20a32597dc
Safuh <Safoh95@windowslive.com>
2019-01-14 11:08:09 -0800

    added sound

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const Game = require("./game");
+import { Howl } from 'howler';
 
 document.addEventListener("DOMContentLoaded", function() {
   const canvasEl = document.getElementById("game-canvas");
@@ -6,13 +7,26 @@ document.addEventListener("DOMContentLoaded", function() {
   canvasEl.width = 1000
   canvasEl.height = 600;
 
+  let sound = new Howl ({
+    src: ['/src/Two.mp3'],
+    buffer: true,
+    loop: true
+  })
+
 
   ctx.fillStyle = 'black';
   ctx.fillRect(0, 0, canvasEl.width, canvasEl.height);
-  const game = new Game({ctx: ctx, canvas: canvasEl, level: 1});
 
-  game.start()
+  document.querySelector('button').addEventListener('click', () => {
+    const game = new Game({ctx: ctx, canvas: canvasEl, level: 1});
+    game.start()
+    // sound.play()
+  })
+
+  // sound.stop()
 });
+
+//python -m SimpleHTTPServer
 
 
 
